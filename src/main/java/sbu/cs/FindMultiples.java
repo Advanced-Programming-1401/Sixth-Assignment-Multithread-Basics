2039626f2d10844e10a976c0a799afcf6618c83c
maryamSoli <maryamsoleimani183@gmail.com>
2023-05-01 11:03:16 -0700

    second one done

--- src/main/java/sbu/cs/FindMultiples.java
+++ src/main/java/sbu/cs/FindMultiples.java
@@ -19,6 +19,11 @@ package sbu.cs;
     Use the tests provided in the test folder to ensure your code works correctly.
  */
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class FindMultiples
 {
 
@@ -29,14 +34,80 @@ public class FindMultiples
     The getSum function should be called at the start of your program.
     New Threads and tasks should be created here.
     */
-    public int getSum(int n) {
+
+    public ArrayList<Integer> dividends = new ArrayList<Integer>();
+    public static int n;
+    int temp = 0;
+    public synchronized void increment(){
+        dividends.add(temp);
+    }
+    Thread divisibleBy3  = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            for (int i=1 ; i<=n ; i++){
+                if (i%3==0){
+                    temp=i;
+                    increment();
+                }
+            }
+        }
+    });
+
+    Thread divisibleBy5  = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            for (int i=1 ; i<=n ; i++){
+                if (i%5==0){
+                    temp=i;
+                    increment();
+                }
+            }
+        }
+    });
+
+    Thread divisibleBy7  = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            for (int i=1 ; i<=n ; i++){
+                if (i%7==0){
+                    temp=i;
+                    increment();
+                }
+            }
+        }
+    });
+
+
+
+    public int getSum() {
         int sum = 0;
+        Set<Integer> set = new HashSet<>(dividends);
+        dividends.clear();
+        dividends.addAll(set);
 
-        // TODO
-
+        for (int i : dividends){
+            sum+=i;
+        }
         return sum;
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+        FindMultiples obj = new FindMultiples();
+        obj.divisibleBy3.start();
+        obj.divisibleBy5.start();
+        obj.divisibleBy7.start();
+        try {
+            obj.divisibleBy3.join();
+            obj.divisibleBy5.join();
+            obj.divisibleBy7.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(obj.getSum());
+
     }
+
 }
