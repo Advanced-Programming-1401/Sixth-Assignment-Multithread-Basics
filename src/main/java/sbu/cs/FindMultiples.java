f29f537386885231c2c49d9f0422265056e76abf
arghavan83 <ar83fadakar@gmail.com>
2023-05-04 16:17:02 +0430

    "secend"

--- src/main/java/sbu/cs/FindMultiples.java
+++ src/main/java/sbu/cs/FindMultiples.java
@@ -23,12 +23,74 @@ import java.util.ArrayList;
 
 public class FindMultiples
 {
+    boolean workDone3=false;
+    boolean workDone5=false;
+    boolean workDone7=false;
+    ArrayList<Integer>total=new ArrayList<>();
+    ArrayList<Integer> counter3 = new ArrayList<>();
+    ArrayList<Integer> counter5 = new ArrayList<>();
+    ArrayList<Integer> counter7 = new ArrayList<>();
+public class Group3 implements Runnable {
 
-    public void group3(int n){
+    public int n;
+    public Group3(int n){
+        this.n=n;
+
+    }
+
+    @Override
+    public void run(){
+
         for(int i=1;i<=n;i++){
+            if(i%3==0){
+                counter3.add(i);
+            }
 
         }
+        workDone3=true;
 
+
+    }
+}
+    public class Group5 implements Runnable {
+        public int n;
+        public Group5(int n){
+            this.n=n;
+        }
+
+        @Override
+        public void run(){
+
+            for(int i=1;i<=n;i++){
+                if(i%5==0){
+                    counter5.add(i);
+                }
+
+            }
+            workDone5=true;
+
+
+        }
+    }
+    public class Group7 implements Runnable {
+        public int n;
+        public Group7(int n){
+            this.n=n;
+        }
+
+        @Override
+        public void run(){
+
+            for(int i=1;i<=n;i++){
+                if(i%7==0){
+                    counter7.add(i);
+                }
+
+            }
+            workDone7=true;
+
+
+        }
     }
 
     // TODO create the required multithreading class/classes using your preferred method.
@@ -40,6 +102,46 @@ public class FindMultiples
     */
     public int getSum(int n) {
         int sum = 0;
+        Thread thread1=new Thread(new Group3(n));
+        Thread thread2=new Thread(new Group5(n));
+        Thread thread3=new Thread(new Group7(n));
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        if(workDone3 && workDone5 && workDone7){
+            for (int int1:counter3){
+                total.add(int1);
+                sum+=int1;
+            }
+            for(int int1:counter5){
+                boolean unique=true;
+                for (int int2:total){
+                    if(int1==int2){
+                        unique=false;
+                    }
+                }
+                if(unique){
+                    total.add(int1);
+                    sum+=int1;
+                }
+            }
+            for(int int1:counter7){
+                boolean unique=true;
+                for (int int2:total){
+                    if(int1==int2){
+                        unique=false;
+                    }
+                }
+                if(unique){
+                    total.add(int1);
+                    sum+=int1;
+                }
+            }
+
+        }
+        for (int i:total){
+            System.out.println(i);
+        }
 
         // TODO
 
@@ -47,5 +149,9 @@ public class FindMultiples
     }
 
     public static void main(String[] args) {
+        FindMultiples findMultiples=new FindMultiples();
+        findMultiples.getSum(9);
+
+
     }
 }
